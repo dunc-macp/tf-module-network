ee3306ef8c949c02d434717bd6d9f719756964a1
dunc-macp <dunc.macp@gmail.com>
2024-06-25 23:47:53 +0100

    Changes

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@
 
 terraform {
   required_version = ">= 1.0"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.55.0"
-    }
-  }
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "5.55.0"
+#     }
+#   }
 }
